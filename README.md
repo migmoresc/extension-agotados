37a7bf9a6918739ea52a1b00a3c7bcf5962cac86
Miguel Moreno <migmoresc@outlook.es>
2023-06-26 08:33:30 +0200

    funciona para coolmod

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # extension-agotados
-No muestra los productos agotados de algunas tiendas
+
+No muestra los productos agotados de algunas tiendas.
+Actualmente solo funciona en la web de coolmod.com
---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// alert("A")
+console.log("script.js")
+window.addEventListener('load', function () {
+
+    // $(".stock.off").parent().parent().parent().parent().parent().parent().parent().parent().css("display", "none");
+    //para coolmod
+    $(".stock.off").parents(".productInfo").css("display", "none");
+    $(".coolheader.coolproductsection").css("display", "none");
+    $(".internal-link").css("display", "none");
+
+    //para pccomponentes
+
+})
